4697603522a92b2b2716e4bbde984793a8851db7
Rampukar <imaryan08@gmail.com>
2022-05-06 00:59:20 +0530

    footer modified

--- footer/footer.js
+++ footer/footer.js
@@ -5,84 +5,44 @@ const footerLogo = () => {
 
 // about us
 const downArrow = () => {
-  document.querySelector("#downArrow").addEventListener("click", () => {
     document.querySelector(".aboutUs").style.display = "block";
     document.querySelector("#downArrow").style.display = "none";
     document.querySelector("#upArrow").style.display = "inline-block";
-  });
+}
 
-  document.querySelector("#upArrow").addEventListener("click", () => {
+const upArrow = () => {
     document.querySelector(".aboutUs").style.display = "none";
     document.querySelector("#downArrow").style.display = "inline-block";
     document.querySelector("#upArrow").style.display = "none";
-  });
-};
+}
 
 // socialHandle
 const downArrow1 = () => {
-  document.querySelector("#downArrow2").addEventListener("click", () => {
     document.querySelector(".socialHandle").style.display = "block";
     document.querySelector("#downArrow2").style.display = "none";
     document.querySelector("#upArrow2").style.display = "inline-block";
-  });
+  };
 
-  document.querySelector("#upArrow2").addEventListener("click", () => {
+const upArrow1 = () => {
     document.querySelector(".socialHandle").style.display = "none";
     document.querySelector("#downArrow2").style.display = "inline-block";
     document.querySelector("#upArrow2").style.display = "none";
-  });
 };
+
 
 // policy
 const downArrow2 = () => {
-  document.querySelector("#downArrow1").addEventListener("click", () => {
     document.querySelector(".policy").style.display = "block";
     document.querySelector("#downArrow1").style.display = "none";
     document.querySelector("#upArrow1").style.display = "inline-block";
-  });
+  };
 
-  document.querySelector("#upArrow1").addEventListener("click", () => {
+  const upArrow2 = () => {
     document.querySelector(".policy").style.display = "none";
     document.querySelector("#downArrow1").style.display = "inline-block";
     document.querySelector("#upArrow1").style.display = "none";
-  });
 };
 
-//! about us
-// document.querySelector("#downArrow").addEventListener("click", () => {
-//   document.querySelector(".aboutUs").style.display = "block";
-//   document.querySelector("#downArrow").style.display = "none";
-//   document.querySelector("#upArrow").style.display = "inline-block";
-// });
 
-// document.querySelector("#upArrow").addEventListener("click", () => {
-//   document.querySelector(".aboutUs").style.display = "none";
-//   document.querySelector("#downArrow").style.display = "inline-block";
-//   document.querySelector("#upArrow").style.display = "none";
-// });
 
-//! socialHandle
-// document.querySelector("#downArrow2").addEventListener("click", () => {
-//   document.querySelector(".socialHandle").style.display = "block";
-//   document.querySelector("#downArrow2").style.display = "none";
-//   document.querySelector("#upArrow2").style.display = "inline-block";
-// });
 
-// document.querySelector("#upArrow2").addEventListener("click", () => {
-//   document.querySelector(".socialHandle").style.display = "none";
-//   document.querySelector("#downArrow2").style.display = "inline-block";
-//   document.querySelector("#upArrow2").style.display = "none";
-// });
-
-//! policy
-// document.querySelector("#downArrow1").addEventListener("click", () => {
-//   document.querySelector(".policy").style.display = "block";
-//   document.querySelector("#downArrow1").style.display = "none";
-//   document.querySelector("#upArrow1").style.display = "inline-block";
-// });
-
-// document.querySelector("#upArrow1").addEventListener("click", () => {
-//   document.querySelector(".policy").style.display = "none";
-//   document.querySelector("#downArrow1").style.display = "inline-block";
-//   document.querySelector("#upArrow1").style.display = "none";
-// });
