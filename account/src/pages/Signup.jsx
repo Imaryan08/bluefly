0fa9fccda112c095ce351036108dcc2726d9c443
Surajit Dhar <surajitgokarna@gmail.com>
2022-05-05 18:36:07 +0530

    login done

--- account/src/pages/Signup.jsx
+++ account/src/pages/Signup.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { slideData } from '../components/SlideData';
 import { useState,useEffect } from 'react';
 import "./Signup.css";
+import { BelowData } from '../components/BelowData';
 
 
 const Container = styled.div`
@@ -12,6 +13,48 @@ const Container = styled.div`
     color:white;
    text-align: center;
    padding: 1px ;
+`
+const Heading = styled.h1`
+    font-family: Tiemann ,serif;
+    font-weight: 500;
+    letter-spacing: 0em;
+    line-height: 1;
+    font-size: 37px;
+    text-transform: uppercase;
+    display: block;
+    margin: 19px;
+    margin-left: 46%;
+    
+`
+const Inputitem = styled.div`
+ 
+  height:310px;
+  width:450px;
+  margin-top: 40px;
+  margin-left: 34%;
+`
+const Button = styled.button`
+    height:45px;
+    width:99%;
+    margin-top: 7%;
+    padding: 8px 10px;
+    background-color: black;
+    letter-spacing: 0.35em;
+    color:white;
+    font-weight: bolder;
+`
+const Create = styled.p`
+   font-size: 15.8px;
+   padding-top:3px;
+   padding-left: 38.4%;
+`
+const Offer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3,1fr);
+  grid-gap:7px;
+  margin-top: 25px;
+  text-align: center;
+  margin-bottom: 50px;
 `
 
 const Signup = () => {
@@ -38,6 +81,7 @@ const Signup = () => {
  },[currentSlide])
 
   return (
+    <div className='login-page'>
     <Container className='slide'>
       {slideData.map((slide,index) => {
         return (
@@ -53,6 +97,41 @@ const Signup = () => {
         )
       })}
     </Container>
+    <Heading>LOGIN</Heading>
+    <Inputitem>
+    <div>
+    <label className='l1'>EMAIL</label>
+     <br/>
+     <input type="text" className='input1'/>
+
+    </div>
+    <div  className='pass'>
+    <label className='l1'>PASSWORD</label>
+    <label className='l1'>Forgot password?</label>
+    </div>
+    
+     <input type="text" className='input1'/>
+     <Button>SIGN IN</Button><br/>
+     <Create>Create account</Create>
+
+    </Inputitem>
+    <hr/>
+    <Offer>
+      {BelowData.map((elem) => {
+        return(
+          <div key={elem.id}>
+              <img src={elem.image} className="pics"/>
+              <p className='p1'>{elem.disp}</p>
+              <p>{elem.short}</p>
+              <button className='btn-modi'>{elem.btn}</button>
+          </div>
+         
+        )
+      })}
+
+    </Offer>
+   
+    </div>
   )
 }
 
