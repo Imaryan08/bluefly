43e710e7521371ece0b6d17ee81ae79cdff203d0
Surajit Dhar <surajitgokarna@gmail.com>
2022-05-07 01:36:52 +0530

    cart

--- account/src/App.jsx
+++ account/src/App.jsx
@@ -3,17 +3,26 @@ import './App.css';
 import Signup from './pages/Signup';
 import {Routes,Route, Router} from "react-router-dom";
 import Register from './pages/Register';
+import Cart from './pages/Cart';
+import {useState} from 'react';
+import React from 'react';
 
 function App() {
+
+  const [show , setShow] = useState(false);
+
   return (
     <div className="App">
+       {/* <button onClick={()=> {setShow(!show);}}>Cart</button> */}
+       {/* {show ? <Cart/> : null} */}
       
-      <Routes>
+      
+      {/* <Routes>
         <Route path="/" element={<Signup/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
-      </Routes>
+      </Routes> */}
       
-     
+     <Cart/>
     </div>
   );
 }
