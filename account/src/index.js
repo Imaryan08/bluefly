99632e90929c9083d0964547c34a644814983f5c
Rampukar <imaryan08@gmail.com>
2022-05-08 17:50:35 +0530

    login signup page added

--- account/src/index.js
+++ account/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { Context } from './components/Context';
+import { Context } from './components/Context'; 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
